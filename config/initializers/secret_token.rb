890cbceccc631c36903587bfb5ffb9a0b73686f0
Jens Bissinger <mail@jens-bissinger.de>
2014-04-01 08:41:58 +0200

    Boostrap rails.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CocoaTreeStore::Application.config.secret_key_base = 'c02a2e415c3640a54b45f3f69e3514359017d1f41f4d613db1b17e22c468adc7a080f289b3171dd6ba81c611bab4d9d742981fe0ac53476293163385d12bf45e'
