69c320b922d2346badcd0c27097011a8e1739b73
Jens Bissinger <mail@jens-bissinger.de>
2014-04-07 19:02:31 +0200

    Add UpdateCocoaPodCategoryJob.

---
+++ app/jobs/update_cocoa_pod_category_job.rb
@@ -0,0 +1,16 @@
+class UpdateCocoaPodCategoryJob
+  def run
+    categories = CocoaPodYAMLCategories.new
+    CocoaPod.all.find_each do |cocoa_pod|
+      if category_name = categories[cocoa_pod.name]
+        category = cocoa_pod.cocoa_pod_category
+        if !category || category.name != category_name
+          Rails.logger.info "Assigning category #{category_name} to #{cocoa_pod.name}"
+          new_category = CocoaPodCategory.find_or_create_by_name(category_name)
+          cocoa_pod.cocoa_pod_category = new_category
+          cocoa_pod.save
+        end
+      end
+    end
+  end
+end
