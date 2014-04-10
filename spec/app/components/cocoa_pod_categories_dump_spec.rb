a81dc2439a3b7e1535d6f096581770f7cb178e42
Jens Bissinger <mail@jens-bissinger.de>
2014-04-10 19:04:32 +0200

    Test that category dump contains comments.

--- spec/app/components/cocoa_pod_categories_dump_spec.rb
+++ spec/app/components/cocoa_pod_categories_dump_spec.rb
@@ -21,10 +21,13 @@ describe CocoaPodCategoriesDump do
                {:name => "another-other-pod", :category=>"", :comment=>""}]
   end
 
-  it 'dumps file' do
+  it 'dumps file with comments' do
     path = root_path('tmp', 'rspec-CocoaPodCategoriesDump.json')
     subject.dump(path)
-    JSON.parse(File.read(path)).should == {
+    content = File.read(path)
+    content.should include('This is the example.')
+    content.should include('This is another one.')
+    JSON.parse(content).should == {
       "example-pod" => "example-category",
       "another-pod" => "other-category",
       "another-other-pod" => ""
