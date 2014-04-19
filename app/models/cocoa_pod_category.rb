e20dd7a7c60a0111a25aaaa225c87b37d88204d2
Jens Bissinger <mail@jens-bissinger.de>
2014-04-19 11:59:34 +0200

    Set categories serializable hash return name + cocoa pods count.

--- app/models/cocoa_pod_category.rb
+++ app/models/cocoa_pod_category.rb
@@ -3,4 +3,11 @@ class CocoaPodCategory < ActiveRecord::Base
   
   validates_uniqueness_of :name
   validates_presence_of :name
+
+  def serializable_hash context=nil
+    {
+      'name' => name,
+      'cocoa_pods_count' => cocoa_pods.count
+    }
+  end
 end
