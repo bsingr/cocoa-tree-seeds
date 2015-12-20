6880b795059f8790bd778885e4997ea21f14e04e
Jens Bissinger <mail@jens-bissinger.de>
2015-12-20 18:21:38 +0100

    Fix categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,9 +2,9 @@ class CategoriesController < ApplicationController
   caches_page :index
 
   def index
-    categories = CocoaPodCategory.all.to_a
+    categories = CocoaPodCategory.all.to_a.map(&:last).map(&:serializable_hash)
     respond_to do |format|
-      format.mpac { render :text => MessagePack.dump(categories.map(&:last).map(&:serializable_hash)) }
+      format.mpac { render :text => MessagePack.dump(categories) }
       format.json { render json: categories }
     end
   end
